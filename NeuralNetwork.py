Missing gpu acceleration and fixing bugs on loss, backpropagation and optimization

NeuralNetwork.py
@@ -1,15 +1,61 @@
+import torch
 from torch import nn
-class NeuralNetwork(nn.Module):
+from torch import optim
+import numpy as np
+
+loss_fn = nn.MSELoss()
+
+class StockPriceNeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         self.non_linear_Sequential = nn.Sequential(
-            nn.Linear(512, 128),
+            nn.Flatten(),
+            nn.Linear(210, 64),
             nn.ReLU(),
-            nn.Linear(128, 32),
+            nn.Linear(64, 16),
             nn.ReLU(),
-            nn.Linear(32, 1),
+            nn.Linear(16, 1),
         )
         pass
     def forward(self, x):
-        y = nn.non_linear_Sequential
+        y = self.non_linear_Sequential(x)
         return y
+
+def train_loop(dataloader: torch.utils.data.DataLoader, model: nn.Module, batch_size=64, learning_rate=1e-3):
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    size = len(dataloader)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        # Compute prediction and loss
+        pred = model(X)
+        y = np.reshape(y, (-1, 1))
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * batch_size + len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test_loop(dataloader: torch.utils.data.DataLoader, model: nn.Module):
+    # Set the model to evaluation mode - important for batch normalization and dropout layers
+    # Unnecessary in this situation but added for best practices
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
+    # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")

main.py
@@ -3,11 +3,18 @@ import torch
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from pandas.core.interchange.dataframe_protocol import DataFrame
+from NeuralNetwork import StockPriceNeuralNetwork, train_loop, test_loop
 
-EPOCHS = 10
+#Rules
+N_EPOCHS = 100
+BATCH_SIZE = 128
+LEARNING_RATE = 0.02
 PATH = kagglehub.dataset_download("mattiuzc/stock-exchange-data")
 PATH += "/indexProcessed.csv"
+# DEVICE = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+# print(f"Using {DEVICE} device")
+# torch.set_default_device(DEVICE)
+# torch.set_default_dtype(torch.float32)
 
 #Preprocessing
 #Params shape n * 30 days * 9 columns on csv
@@ -26,25 +33,37 @@ split_breakpoint = round(len(split_time_series) * 0.8)
 
 train_X = np.array(split_time_series[0:(split_breakpoint-1)])
 train_Y = np.ndarray(shape=(len(train_X)))
+m=0
 for v in split_time_series[1:split_breakpoint]:
-    np.append(train_Y, v["Adj Close"].tolist()[0])
+    train_Y[m] = v["Adj Close"].tolist()[0]
+    m+=1
 
 test_X = np.array(split_time_series[split_breakpoint:])
 test_Y = np.ndarray(shape=(len(test_X)))
+m=0
 for v in split_time_series[split_breakpoint+1:]:
-    np.append(test_Y, v["Adj Close"].tolist()[0])
-np.append(test_Y, uncomplete_month["Adj Close"].tolist()[0])
+    test_Y[m] = v["Adj Close"].tolist()[0]
+    m += 1
+test_Y[m] = uncomplete_month["Adj Close"].tolist()[0]
 
 print(f"Dataframe shape: {np.shape(split_time_series)}"
       f"\nTrain shape: {np.shape(train_X)} Validate shape: {np.shape(test_X)}"
       f"\nTrain Y shape: {np.shape(train_Y)} Validate Y shape: {np.shape(test_Y)}")
 
-train_X = torch.from_numpy(train_X)
-train_Y = torch.from_numpy(train_Y)
-test_X = torch.from_numpy(test_X)
-test_Y = torch.from_numpy(test_Y)
+train_X = torch.from_numpy(train_X).to(dtype=torch.float32)#.to(DEVICE)
+train_Y = torch.from_numpy(train_Y).to(dtype=torch.float32)#.to(DEVICE)
+test_X = torch.from_numpy(test_X).to(dtype=torch.float32)#.to(DEVICE)
+test_Y = torch.from_numpy(test_Y).to(dtype=torch.float32)#.to(DEVICE)
+
+trainset = torch.utils.data.TensorDataset(train_X, train_Y)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True)#, generator=torch.Generator(device=DEVICE))
+testset = torch.utils.data.TensorDataset(test_X, test_Y)
+testloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True)#, generator=torch.Generator(device=DEVICE))
 
 #Model
-device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
-print(f"Using {device} device")
+model = StockPriceNeuralNetwork()
+for t in range(N_EPOCHS):
+    print(f"Epoch {t}\n-------------------------------")
+    train_loop(trainloader, model, batch_size=BATCH_SIZE, learning_rate=LEARNING_RATE)
+    test_loop(testloader, model)
 